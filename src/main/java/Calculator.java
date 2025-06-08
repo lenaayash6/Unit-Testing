e2e132793218d1064c2d126afe643d5bcb84bace
Lena Ayash <lena@800storagedubai.com>
2025-06-08 03:02:50 +0300

    first JUnit code

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Divide by zero");
+        }
+        return a / b;
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+    Calculator calculator;
+    @BeforeEach
+     void setUp() {
+        calculator = new Calculator();
+    }
+    @Test
+     void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+    @Test
+    void testDivideByOne() {
+        assertEquals(2, calculator.divide(2, 1));
+    }
+    @Test
+     void testDivide() {
+        assertEquals(2, calculator.divide(6, 3));
+    }
+    @Test
+     void testDivideByZero() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            calculator.divide(10, 0);
+        });
+        assertEquals("Divide by zero", exception.getMessage());
+    }
+    @AfterEach
+    void tearDown() {
+        System.out.println("test finished");
+    }
+
+}
